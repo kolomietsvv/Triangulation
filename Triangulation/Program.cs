748c38879d6ce7e0c1ae6d3ce323c00ef918a8b7
affter <after-shock@ya.ru>
2017-05-10 09:18:59 +0400

    Some work

--- Triangulation/Program.cs
+++ Triangulation/Program.cs
@@ -24,13 +24,15 @@ namespace ConsoleApp1
 
             var screenPoints = points.Select(p => new PointF(p.X, maxY - p.Y)).ToList();
 
+            var triangles = GetTrianglesFromFile("...\\...\\Resources\\rectan.1.ele", screenPoints);
+
             var img = new Image<Bgr, byte>((int)maxX + 1, (int)maxY + 1);
-            foreach (var edge in edges)
+            foreach (var triangle in triangles)
             {
-                var line = new LineSegment2DF(screenPoints[edge.X], screenPoints[edge.Y]);
-                img.Draw(line, new Bgr(255, 255, 255), 1);
+                img.Draw(triangle, new Bgr(255, 255, 0), -1);
+                img.Draw(triangle, new Bgr(255, 255, 255), 1);
             }
-                        
+
             CvInvoke.Imshow("Triangulation", img);
             CvInvoke.WaitKey();
         }
@@ -41,7 +43,6 @@ namespace ConsoleApp1
             var nodesCount = int.Parse(fileContent[0].Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries)[0]);
             var points = new List<PointF>(nodesCount);
 
-            points.Add(Point.Empty);
             for (int i = 1; i <= nodesCount; i++)
             {
                 var lineContent = fileContent[i].Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
@@ -60,17 +61,35 @@ namespace ConsoleApp1
             var edgesCount = int.Parse(fileContent[0].Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries)[0]);
             var edges = new List<Point>(edgesCount);
 
-            edges.Add(Point.Empty);
             for (int i = 1; i <= edgesCount; i++)
             {
                 var lineContent = fileContent[i].Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
                 var id = int.Parse(lineContent[0]);
                 var pointFrom = int.Parse(lineContent[1]);
                 var pointTo = int.Parse(lineContent[2]);
-                var point = new Point(pointFrom, pointTo);
+                var point = new Point(pointFrom - 1, pointTo - 1);
                 edges.Add(point);
             }
             return edges;
+        }
+
+        private static List<Triangle2DF> GetTrianglesFromFile(string path, List<PointF> points)
+        {
+            var fileContent = File.ReadAllLines(path);
+            var trianglesCount = int.Parse(fileContent[0].Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries)[0]);
+            var triangles = new List<Triangle2DF>(trianglesCount);
+
+            for (int i = 1; i <= trianglesCount; i++)
+            {
+                var lineContent = fileContent[i].Split(new[] { "  " }, StringSplitOptions.RemoveEmptyEntries);
+                var id = int.Parse(lineContent[0]);
+                var point1ID = int.Parse(lineContent[1]);
+                var point2ID = int.Parse(lineContent[2]);
+                var point3ID = int.Parse(lineContent[3]);
+                var triangle = new Triangle2DF(points[point1ID - 1], points[point2ID - 1], points[point3ID - 1]);
+                triangles.Add(triangle);
+            }
+            return triangles;
         }
     }
 }
